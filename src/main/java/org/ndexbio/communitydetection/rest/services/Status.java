c97f292a7c4bf02f02b860a893a3c377fb9f5e8e
Chris Churas <churas.camera@gmail.com>
2019-08-20 17:20:18 -0700

    i think setting contextpath lets one avoid this hardcoding of /cd

--- src/main/java/org/ndexbio/communitydetection/rest/services/Status.java
+++ src/main/java/org/ndexbio/communitydetection/rest/services/Status.java
@@ -23,7 +23,7 @@ import org.ndexbio.communitydetection.rest.engine.CommunityDetectionEngine;
  */
 @Server(
         description = "default",
-        url = "/cd" + Configuration.APPLICATION_PATH
+        url = Configuration.APPLICATION_PATH
         )
 @Path("/")
 public class Status {
