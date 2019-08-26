447e2b834946ec76a73671d924ec7185a32bba38
Chris Churas <churas.camera@gmail.com>
2019-08-26 16:34:48 -0700

    initial revision

---
+++ src/main/java/org/ndexbio/communitydetection/rest/engine/CmdlineCommunityDetectionAlgorithmsFactory.java
@@ -0,0 +1,17 @@
+package org.ndexbio.communitydetection.rest.engine;
+
+import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithms;
+
+/**
+ *
+ * @author churas
+ */
+public class CmdlineCommunityDetectionAlgorithmsFactory implements CommunityDetectionAlgorithmsFactory  {
+
+    @Override
+    public CommunityDetectionAlgorithms getCommunityDetectionAlgorithms() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+}
---
+++ src/main/java/org/ndexbio/communitydetection/rest/engine/CommunityDetectionAlgorithmsFactory.java
@@ -0,0 +1,12 @@
+package org.ndexbio.communitydetection.rest.engine;
+
+import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithms;
+
+/**
+ *
+ * @author churas
+ */
+public interface CommunityDetectionAlgorithmsFactory {
+    
+    public CommunityDetectionAlgorithms getCommunityDetectionAlgorithms();
+}
