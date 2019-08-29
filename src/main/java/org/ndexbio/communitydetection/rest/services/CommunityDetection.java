710f5b27ea33b000e1aa9ad0ef0939676aaa55dd
Chris Churas <churas.camera@gmail.com>
2019-08-29 16:00:13 -0700

    bumped to 0.5.0

--- src/main/java/org/ndexbio/communitydetection/rest/services/CommunityDetection.java
+++ src/main/java/org/ndexbio/communitydetection/rest/services/CommunityDetection.java
@@ -39,7 +39,7 @@ import org.ndexbio.communitydetection.rest.model.exceptions.CommunityDetectionEx
  */
 @OpenAPIDefinition( info = 
     @Info(title = "Community Detection REST service",
-          version = "0.4.0",
+          version = "0.5.0",
           description = "This service lets caller invoke various community detection clustering "
                   + "algorithms\n\n "
                   + "<b>NOTE:</b> This service is experimental. The interface is subject to change.\n" +
