ff4ddbba4f607857fe4f8777a7bd5c1debce6437
Chris Churas <churas.camera@gmail.com>
2021-02-09 17:20:27 -0800

    updated to return errors as was done in old diffusion service

--- src/main/java/org/ndexbio/communitydetection/rest/services/CommunityDetection.java
+++ src/main/java/org/ndexbio/communitydetection/rest/services/CommunityDetection.java
@@ -18,16 +18,21 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.ndexbio.communitydetection.rest.model.CommunityDetectionRequest;
 import org.ndexbio.communitydetection.rest.model.CommunityDetectionResult;
 import org.ndexbio.communitydetection.rest.engine.CommunityDetectionEngine;
+import org.ndexbio.communitydetection.rest.model.CXMateResult;
+import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithm;
 import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithms;
 import org.ndexbio.communitydetection.rest.model.CommunityDetectionResultStatus;
 import org.ndexbio.communitydetection.rest.model.ErrorResponse;
@@ -41,7 +46,7 @@ import org.ndexbio.communitydetection.rest.model.exceptions.CommunityDetectionEx
  */
 @OpenAPIDefinition( info = 
     @Info(title = "Community Detection REST service",
-          version = "0.8.0-SNAPSHOT",
+          version = "0.8.1-SNAPSHOT",
           description = "This service lets caller invoke various community detection clustering "
                   + "algorithms which have been packaged into Docker images. To see what "
                   + "algorithms are supported visit the 'algorithms' endpoint below.\n\n "
@@ -57,9 +62,74 @@ public class CommunityDetection {
     
     static Logger logger = LoggerFactory.getLogger(CommunityDetection.class);
     
+	@GET
+	@Path(Configuration.LEGACY_DIFFUSION_PATH + "/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(hidden = true,
+			   summary = "Legacy endpoint to let user know NOT to use this endpoint",
+			   description="This endpoint exists to maintain consistency with legacy "
+					   + "diffusion service that returns 405 error and json response",
+			   responses = {
+				   @ApiResponse(responseCode = "405", 
+						        description = "The response will be JSON with no data "
+										+ "and a single error denoting this endpoint "
+										+ " should not be used.",
+								content = @Content(mediaType = MediaType.APPLICATION_JSON,
+								schema = @Schema(implementation = CXMateResult.class)))
+			   })
+	public Response legacyDiffusionGet(){
+		CXMateResult result = new CXMateResult("you must use the POST method with this endpoint",
+		null, Response.Status.METHOD_NOT_ALLOWED.getStatusCode(), null);
+		 return Response.status(Response.Status.METHOD_NOT_ALLOWED).type(MediaType.APPLICATION_JSON)
+					.entity(result.asJson()).build();
+	}
+	
+	@DELETE
+	@Path(Configuration.LEGACY_DIFFUSION_PATH + "/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(hidden = true,
+			   summary = "Legacy endpoint to let user know NOT to use this endpoint",
+			   description="This endpoint exists to maintain consistency with legacy "
+					   + "diffusion service that returns 405 error and json response",
+			   responses = {
+				   @ApiResponse(responseCode = "405", 
+						        description = "The response will be JSON with no data "
+										+ "and a single error denoting this endpoint "
+										+ " should not be used.",
+								content = @Content(mediaType = MediaType.APPLICATION_JSON,
+								schema = @Schema(implementation = CXMateResult.class)))
+			   })
+	public Response legacyDiffusionDelete(){
+		CXMateResult result = new CXMateResult("you must use the POST method with this endpoint",
+		null, Response.Status.METHOD_NOT_ALLOWED.getStatusCode(), null);
+		 return Response.status(Response.Status.METHOD_NOT_ALLOWED).type(MediaType.APPLICATION_JSON)
+					.entity(result.asJson()).build();
+	}
+	
+	@PUT
+	@Path(Configuration.LEGACY_DIFFUSION_PATH + "/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(hidden = true,
+			   summary = "Legacy endpoint to let user know NOT to use this endpoint",
+			   description="This endpoint exists to maintain consistency with legacy "
+					   + "diffusion service that returns 405 error and json response",
+			   responses = {
+				   @ApiResponse(responseCode = "405", 
+						        description = "The response will be JSON with no data "
+										+ "and a single error denoting this endpoint "
+										+ " should not be used.",
+								content = @Content(mediaType = MediaType.APPLICATION_JSON,
+								schema = @Schema(implementation = CXMateResult.class)))
+			   })
+	public Response legacyDiffusionPut(){
+		CXMateResult result = new CXMateResult("you must use the POST method with this endpoint",
+		null, Response.Status.METHOD_NOT_ALLOWED.getStatusCode(), null);
+		 return Response.status(Response.Status.METHOD_NOT_ALLOWED).type(MediaType.APPLICATION_JSON)
+					.entity(result.asJson()).build();
+	}
 	
 	@POST
-	@Path(Configuration.V_ONE_PATH + "/" + Configuration.LEGACY_DIFFUSION_PATH + "/")
+	@Path(Configuration.LEGACY_DIFFUSION_PATH + "/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Operation(summary = "Legacy endpoint to run network propagation",
@@ -82,11 +152,12 @@ public class CommunityDetection {
 										+ "will be returned for each node in the CX network, "
 										+ "attributes present in the input network and not "
 										+ "related to heat_diffusion are not guaranteed to be returned.",
-								content = @Content(mediaType = MediaType.APPLICATION_JSON)),
+								content = @Content(mediaType = MediaType.APPLICATION_JSON,
+								schema = @Schema(implementation = CXMateResult.class))),
 				   @ApiResponse(responseCode = "500",
-						        description = "There was an error",
+						        description = "There was an error parsing input or running task",
 				                content = @Content(mediaType = MediaType.APPLICATION_JSON,
-                                  schema = @Schema(implementation = ErrorResponse.class)))
+                                  schema = @Schema(implementation = CXMateResult.class)))
 			   })
 	public Response legacyDiffusion(@RequestBody(description="The body of the request must be a CX network "
 			+ "containing the nodes, edges, and nodeAttributes aspects. There must exist at least one "
@@ -96,23 +167,37 @@ public class CommunityDetection {
 			+ "least one node with an attribute named diffusion_input with value 1.0.)\n" +
 "\n" +
 "All nodes that do not have this nodeAttribute set will be treated as having zero heat.", required = true) final String cxdata,
-			@Parameter(description = "The upper bound on the exponential multiplication performed by diffusion default 0.1") @QueryParam("time") double time,
-			@Parameter(description = "If True, will create a normalized laplacian matrix for diffusion") @QueryParam("normalize_laplacian") boolean normalizeLaplacian,
+			@Parameter(description = "The upper bound on the exponential multiplication performed by diffusion", example="0.1") @QueryParam("time") Double time,
+			@Parameter(description = "If True, will create a normalized laplacian matrix for diffusion") @QueryParam("normalize_laplacian") Boolean normalizeLaplacian,
 			@Parameter(description = "The key diffusion will use to search for heats in the node attributes") @QueryParam("input_attribute_name") final String inputAttributeName,
 			@Parameter(description = "Will be the prefix of the _rank and _heat attributes created by diffusion") @QueryParam("output_attribute_name") final String outputAttributeName) {
+		
 		
 		ObjectMapper omapper = new ObjectMapper();
 		// Create CommunityDetectionRequest passing cxdata in "data" field
 		CommunityDetectionRequest cdr = new CommunityDetectionRequest();
-		try {
-			cdr.setData(omapper.readTree(cxdata));
-		} catch(JsonProcessingException jpe){
-			ErrorResponse er = new ErrorResponse("Error requesting Diffusion", jpe);
-            return Response.serverError().type(MediaType.APPLICATION_JSON).entity(er.asJson()).build();
-		}
-		cdr.setAlgorithm("legacynetworkheatdiffusion");
 		
+		Map<String, String> customParams = new HashMap<>();
+		if (time != null){
+			customParams.put("--time", time.toString());
+		}
+		if (normalizeLaplacian != null){
+			customParams.put("--normalize_laplacian", normalizeLaplacian.toString());
+		}
+		if (inputAttributeName != null){
+			customParams.put("--input_attribute_name", inputAttributeName);
+		}
+		if (inputAttributeName != null){
+			customParams.put("--output_attribute_name", outputAttributeName);
+		}
+		if (customParams.isEmpty() == false){
+			cdr.setCustomParameters(customParams);
+		}
 		try {
+			CommunityDetectionAlgorithm cda = Configuration.getInstance().getDiffusionAlgorithm();
+			cdr.setAlgorithm(cda.getName());
+			cdr.setData(omapper.readTree(cxdata));
+			
 			CommunityDetectionEngine engine = Configuration.getInstance().getCommunityDetectionEngine();
 				if (engine == null){
 					throw new NullPointerException("CommunityDetection Engine not loaded");
@@ -125,22 +210,29 @@ public class CommunityDetection {
 			// Wait for completion by invoking status once per second
 			CommunityDetectionResult cRes = engine.getResult(id);
 			while (cRes.getProgress() < 100){				
-				Thread.sleep(1000);
+				Thread.sleep(100);
 				cRes = engine.getResult(id);
 			}
 			if (cRes.getStatus().equals(CommunityDetectionResult.COMPLETE_STATUS)){
-				return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(cRes.getResult()).build();
+				
+				return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON)
+						.entity(omapper.writeValueAsString(cRes.getResult())).build();
 			}
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(cRes.getResult()).build();
-
+            return Response.serverError().type(MediaType.APPLICATION_JSON).entity(cRes.getResult()).build();
+		} catch(JsonProcessingException jpe){
+			CXMateResult er = new CXMateResult("Error parsing input data", cdr.getAlgorithm(),
+			Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), jpe);
+            return Response.serverError().type(MediaType.APPLICATION_JSON).entity(er.asJson()).build();
 		} catch(CommunityDetectionBadRequestException breq){
             ErrorResponse er = breq.getErrorResponse();
             if (er == null){
                 er = new ErrorResponse("Bad request received", breq);
             }
-            return Response.serverError().type(MediaType.APPLICATION_JSON).entity(er.asJson()).build();
-		}catch(Exception ex){
-            ErrorResponse er = new ErrorResponse("Error requesting CommunityDetection", ex);
+			CXMateResult cError = new CXMateResult(cdr.getAlgorithm(), er);
+            return Response.serverError().type(MediaType.APPLICATION_JSON).entity(cError.asJson()).build();
+		} catch(Exception ex){
+			CXMateResult er = new CXMateResult("Error running diffusion", cdr.getAlgorithm(),
+			Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), ex);
             return Response.serverError().type(MediaType.APPLICATION_JSON).entity(er.asJson()).build();
         }
 	}
