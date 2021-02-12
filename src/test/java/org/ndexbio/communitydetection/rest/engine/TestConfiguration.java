d2f8bb5176918ee477d7e317ef378db4c2547a7b
Chris Churas <churas.camera@gmail.com>
2021-02-11 17:02:55 -0800

    added test of new diffusion code in Configuration

--- src/test/java/org/ndexbio/communitydetection/rest/engine/TestConfiguration.java
+++ src/test/java/org/ndexbio/communitydetection/rest/engine/TestConfiguration.java
@@ -1,13 +1,17 @@
 package org.ndexbio.communitydetection.rest.engine;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Properties;
 import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithm;
+import org.ndexbio.communitydetection.rest.model.CommunityDetectionAlgorithms;
 import org.ndexbio.communitydetection.rest.model.exceptions.CommunityDetectionException;
 import org.ndexbio.communitydetection.rest.services.Configuration;
 
@@ -56,6 +60,8 @@ public class TestConfiguration {
             assertNull(config.getCommunityDetectionEngine());
             assertEquals(1, config.getNumberWorkers());
             assertEquals("docker", config.getDockerCommand());
+			assertEquals(null, config.getDiffusionAlgorithm());
+			assertEquals(100, config.getDiffusionPollingDelay());
             
             assertEquals(null, config.getAlgorithms());
         } finally {
@@ -103,6 +109,53 @@ public class TestConfiguration {
             assertNull(config.getCommunityDetectionEngine());
             assertEquals(180, config.getAlgorithmTimeOut());
             assertEquals(null, config.getAlgorithms());
+        } finally {
+            _folder.delete();
+        }
+    }
+	
+	@Test
+    public void testParseAlgorithmMapWithDiffusion() throws CommunityDetectionException, IOException {
+        File tempDir = _folder.newFolder();
+        try {
+            File configFile = new File(tempDir.getAbsolutePath() + File.separator + "conf");
+            File taskDir = new File(tempDir.getAbsolutePath() + File.separator + "tasks");
+			File algoConfig = new File(tempDir.getAbsolutePath() + File.separator + "algo.json");
+			CommunityDetectionAlgorithms cdAlgos = new CommunityDetectionAlgorithms();
+			CommunityDetectionAlgorithm someAlgo = new CommunityDetectionAlgorithm();
+			someAlgo.setInputDataFormat("EDGELISTV2");
+			someAlgo.setOutputDataFormat("COMMUNITYDETECTRESULTV2");
+			someAlgo.setName("foo");
+			someAlgo.setDisplayName("Foo");
+			CommunityDetectionAlgorithm diffAlgo = new CommunityDetectionAlgorithm();
+			diffAlgo.setInputDataFormat("CXMATE_INPUT");
+			diffAlgo.setOutputDataFormat("CXMATE_OUTPUT");
+			diffAlgo.setName("legacydiffusion");
+			diffAlgo.setDisplayName("Legacy Diffusion");
+			LinkedHashMap<String, CommunityDetectionAlgorithm> algoList = new LinkedHashMap<>();
+			algoList.put(someAlgo.getName(), someAlgo);
+			algoList.put(diffAlgo.getName(), diffAlgo);
+			cdAlgos.setAlgorithms(algoList);
+			ObjectMapper oMapper = new ObjectMapper();
+			oMapper.writeValue(algoConfig, cdAlgos);
+			
+			
+            Properties props = new Properties();
+            props.setProperty(Configuration.TASK_DIR, taskDir.getAbsolutePath());
+            props.setProperty(Configuration.ALGORITHM_MAP, algoConfig.getAbsolutePath());
+            FileOutputStream fos = new FileOutputStream(configFile);
+            props.store(fos, "hello");
+            fos.flush();
+            fos.close();
+            Configuration.setAlternateConfigurationFile(configFile.getAbsolutePath());
+            Configuration config = Configuration.reloadConfiguration();
+            assertEquals(taskDir.getAbsolutePath(), config.getTaskDirectory());
+            assertNull(config.getCommunityDetectionEngine());
+            assertEquals(180, config.getAlgorithmTimeOut());
+			CommunityDetectionAlgorithms resAlgos = config.getAlgorithms();
+			assertEquals(2, resAlgos.getAlgorithms().size());
+			CommunityDetectionAlgorithm dAlgo = config.getDiffusionAlgorithm();
+			assertEquals("legacydiffusion", dAlgo.getName());
         } finally {
             _folder.delete();
         }
